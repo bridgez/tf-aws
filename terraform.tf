9393a4ae802ba41269f3d660103f0037fd9c1651
bridgez <efool@qq.com>
2025-11-12 17:21:12 +0800

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,19 @@
+terraform {
+
+  cloud {
+    organization = "xpfool-org"
+
+    workspaces {
+      name = "HCP-EC2"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.31.0"
+    }
+  }
+
+  required_version = "~> 1.2"
+}
