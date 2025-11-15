f47a5a12a5e367c482dda9aadc76b8629aa87cff
bridgez <efool@qq.com>
2025-11-15 11:28:06 +0800

    Update outputs.tf

--- EC2/outputs.tf
+++ EC2/outputs.tf
@@ -1,5 +1,17 @@
+# Bastion 跳板机公有 IP（本地连接用）
+output "bastion_public_ip" {
+  description = "Bastion 跳板机公有 IP"
+  value       = aws_instance.bastion.public_ip
+}
 
-output "instance_hostname" {
-  description = "Private DNS name of the EC2 instance."
-  value       = aws_instance.app_server.private_dns
+# 内网 EC2 私有 IP（通过 Bastion 访问用）
+output "app_server_private_ip" {
+  description = "内网 EC2（app_server）私有 IP"
+  value       = aws_instance.app_server.private_ip
+}
+
+# VPC 默认安全组 ID（便于后续排查）
+output "default_security_group_id" {
+  description = "VPC 默认安全组 ID"
+  value       = module.vpc.default_security_group_id
 }
