53a4efcd03b40298bc4100447b864a751bb98a1a
bridgez <efool@qq.com>
2025-11-15 11:51:51 +0800

    Update variables.tf

--- EC2/variables.tf
+++ EC2/variables.tf
@@ -1,3 +1,13 @@
+variable "AWS_ACCESS_KEY_ID" {
+  description = "Value of the AWS_ACCESS_KEY_ID."
+  type        = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "Value of the AWS_SECRET_ACCESS_KEY."
+  type        = string
+}
+
 variable "instance_name" {
   description = "Value of the EC2 instance's Name tag."
   type        = string
